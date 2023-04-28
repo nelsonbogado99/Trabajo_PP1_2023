1dacc5429b1bd4a8285e2542ec25b1a39fc9a4ce
Nelson <nelosonbrizue99@gmail.com>
2023-04-28 18:04:45 -0400

    Commit #3 PrimeraParcial No Finalizada

--- TutoDjango/mysite/reserva/admin.py
+++ TutoDjango/mysite/reserva/admin.py
@@ -1,3 +1,9 @@
+
 from django.contrib import admin
 
-# Register your models here.
+from .models import Hotel
+from .models import Reserva
+
+admin.site.register(Hotel)
+
+admin.site.register(Reserva)
