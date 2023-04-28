3afeca11dda9af22dc16c9cb40c12d345186fb97
Nelson <nelosonbrizue99@gmail.com>
2023-04-28 17:28:50 -0400

    Ejercicio #2 PrimeraParcial Finalizada

--- TutoDjango/mysite/reserva/models.py
+++ TutoDjango/mysite/reserva/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class Hotel(models.Model): 
+    nombre = models.CharField(max_length=60)
+    direccion = models.CharField(max_length=100)
+    ciudad = models.CharField(max_length=50)
+    telefono = models.CharField(max_length=20)
+    email = models.EmailField()
+
+class Reserva(models.Model):
+    fechaEntrada = models.DateField()
+    fechaSalida = models.DateField()
+    N_huespedes = models.PositiveBigIntegerField()
+    tipoAbitacion = models.CharField(max_length=30)
+    precioTotal = models.CharField(max_length=100)
+    estadoReserva = models.CharField(max_length=60)
