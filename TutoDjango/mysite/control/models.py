96c6a6e62f7f5e0394aa6dbf15b7a68523020c32
nelsonbogado99 <nelosonbrizue99@gmail.com>
2023-06-16 21:56:16 -0400

    Segundo ejercicio terminado

--- TutoDjango/mysite/control/models.py
+++ TutoDjango/mysite/control/models.py
@@ -1,3 +1,36 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class Empleado(models.Model):
+    
+    identificador = models.IntegerField(unique=True)
+    nombre = models.CharField(max_length=150)
+    departamento = models.CharField(max_length=80)
+    fecha_inicio = models.DateField(default=timezone.now)
+    dias_trabajo = models.CharField(max_length=80)
+    
+    TURNO_OPCIONES = (
+        ('Dia', 'Dia'),
+        ('Tarde', 'Tarde'),
+        ('Noche', 'Noche'),
+    )
+
+    turno = models.CharField(max_length=5, choices=TURNO_OPCIONES)
+    horario_entrada = models.TimeField()
+    horario_salida = models.TimeField()
+
+
+
+class Jornada(models.Model):
+    
+    empleado = models.ForeignKey(Empleado, on_delete=models.CASCADE)
+    fecha = models.DateTimeField()
+    
+    MARCACION_OPCIONES  = (
+        ('Entrada', 'Entrada'),
+        ('Salida', 'Salida'),
+    )
+    
+    tipo_marcacion = models.CharField(max_length=7, choices=MARCACION_OPCIONES)
+
+   
